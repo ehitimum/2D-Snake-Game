14866a01e6843c6e8b6c16b8a965fe7df7df0a9b
Ehtimam <70962870+Ehtimam@users.noreply.github.com>
2020-09-16 01:23:18 +0600

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,8 @@
+
+public class Constants {
+	public static final int WIDTH = 500, HEIGHT = 500  ;
+	public static final int UNIT = 10;
+	public static final int GAME_UNIT= ( WIDTH * HEIGHT )/ UNIT ;
+	static final int SPEED = 75;
+	 
+}
